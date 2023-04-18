500f393a9e1295104d5e586b3590d97b117d2ef9
Antonis <107072477+vlantonakos@users.noreply.github.com>
2023-04-18 14:03:23 +0300

    Add files via upload

---
+++ similarity-api/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import Paragraph from '../components/ui/Paragraph'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main >
+      
+    </main>
+  )
+}
